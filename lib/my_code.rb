ca23c1995530d119ba3a2ce8ea7c6f37cd5b67da
Trevor Jones <tjones009@yahoo.com>
2020-03-04 17:41:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+def map(array)
+  
+  new = []
+  i = 0
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1
+  end 
+  new
+end
+
+def reduce(array, SV=NIL)
+    
+    
+    
